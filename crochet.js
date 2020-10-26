5c56bcdeb76dd9ba18c180ba04300d5e44e6c267
greendragon5000 <52335634+greendragon5000@users.noreply.github.com>
2020-10-27 10:18:46 +1100

    Added crochet circle

--- crochet.js
+++ crochet.js
@@ -7,6 +7,24 @@ C Daniel Vale 2020. All rights reserved.
 const br = "<br>"
 var rows = 0;
 
+// Form data
+var testPatternWidthInStitches = NaN;
+var testPatternWidthMeasure = NaN;
+var testPatternLengthInRows = NaN;
+var testPatternLengthMeasure = NaN;
+var testPatternYarnUsed = NaN;
+var testPatternYarnUsedGrams = NaN;
+var shape = "";
+var shapeRadius = NaN;
+
+// calculate ratios from the test pattern
+var stitchesPerCm = NaN;
+var rowsPerCm = NaN;
+var stitchLengthToWidthRatio = NaN;
+var testPatternTotalStitches = NaN;
+var testPatternYarnPerStitch = NaN;
+	
+
 function load() {
 	console.log("loaded");
 	
@@ -15,37 +33,8 @@ function load() {
 	patternPanel.style.display = "none";
 }
 
-function generate() {
-	console.log("generate");
-	
-	// collect test pattern data from the form
-	let testPatternWidthInStitches = document.forms["test-pattern"]["test-width-stitches"].value;
-	let testPatternWidthMeasure = document.forms["test-pattern"]["test-width-measure"].value;
-	let testPatternLengthInRows = document.forms["test-pattern"]["test-length-rows"].value;
-	let testPatternLengthMeasure = document.forms["test-pattern"]["test-length-measure"].value;
-	let testPatternYarnUsed = document.forms["test-pattern"]["test-yarn-used"].value;
-	let testPatternYarnUsedGrams = document.forms["test-pattern"]["test-yarn-used-grams"].value;
-	
-	console.log("Test pattern width: " + testPatternWidthInStitches + " stitches.");
-	console.log("Test pattern width: " + testPatternWidthInStitches + " mm.");
-	console.log("Test pattern length: " + testPatternWidthInStitches + " rows.");
-	console.log("Test pattern length: " + testPatternWidthInStitches + " mm.");
-	console.log("Test pattern yarn used: " + testPatternYarnUsed + " m.");
-	console.log("Test pattern yarn used: " + testPatternYarnUsedGrams + " g.");
-	
-	// calculate ratios from the test pattern
-	let stitchesPerCm = testPatternWidthInStitches / testPatternWidthMeasure * 10;
-	let rowsPerCm = testPatternLengthInRows / testPatternLengthMeasure * 10;
-	let stitchLengthToWidthRatio = (testPatternWidthMeasure / testPatternWidthInStitches) / (testPatternLengthMeasure / testPatternLengthInRows);
-	
-	console.log("Test pattern stitches per cm: " + stitchesPerCm);
-	console.log("Test pattern rows per cm: " + rowsPerCm);
-	console.log("Test pattern stitch length to width ratio: " + stitchLengthToWidthRatio);
-	
-	// get the target shape details
-	// for now, only a sphere. TODO: other shapes
-	let shape = document.forms["shape"]["shape"].value;
-	let shapeRadius = document.forms["shape"]["radius"].value;
+function generateSphere() {
+	console.log("generate sphere");
 	
 	let radiusInCm = shapeRadius;
 	let diameterInCm = shapeRadius*2;
@@ -57,15 +46,13 @@ function generate() {
 	rows = Math.round(circumfrenceInCm / 2 * rowsPerCm);
 	console.log("rows: " + rows);
 	
-	// get the pattern panel
-	let patternPanel = document.getElementById("pattern");
 	
 	let pattern = `
 <div id="row-1" class="pattern-row" progress="none" onclick="focusRow(event)">
-1 | 6 | magic circle 6 sc
+row 1: magic circle 6 sc (6)
 </div>
 <div id="row-2" class="pattern-row" progress="none" onclick="focusRow(event)">
-2 | 12 | inc in each stitch around
+row 2: inc in each stitch around (12)
 </div>
 `;
 	
@@ -240,13 +227,212 @@ function generate() {
 	pattern += "total stitches: " + totalStitches + br;
 	
 	// estimate total yarn
-	let testPatternTotalStitches = testPatternLengthInRows * testPatternWidthInStitches;
-	let testPatternYarnPerStitch = testPatternYarnUsed / testPatternTotalStitches;
 	let estimateYarnRequired = totalStitches * testPatternYarnPerStitch;
 	let estimateYarnRequiredGrams = totalStitches / testPatternTotalStitches * testPatternYarnUsedGrams
 	pattern += "Estimated yarn required: " + Math.round(estimateYarnRequired) + " m" + br;
 	pattern += "Estimated yarn required: " + Math.round(estimateYarnRequiredGrams) + " g" + br;
 	
+	return pattern;
+}
+
+function generateCircle() {
+	console.log("generate circle");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	let radiusInCm = shapeRadius;
+	let diameterInCm = shapeRadius*2;
+	let circumfrenceInCm = 2.0 * Math.PI * radiusInCm;
+
+	console.log("circumfrence: " + circumfrenceInCm + " cm");
+	
+	// figure the number of rows 
+	rows = Math.round(radiusInCm * rowsPerCm);
+	console.log("rows: " + rows);
+	
+	
+	let pattern = `
+<div id="row-1" class="pattern-row" progress="none" onclick="focusRow(event)">
+row 1: magic circle 6 sc (6)
+</div>
+<div id="row-2" class="pattern-row" progress="none" onclick="focusRow(event)">
+row 2: inc in each stitch around (12)
+</div>
+`;
+	
+	
+	let previousRowStitches = 12;
+	let totalStitches = 18;
+	let targetIncrementStitches = 0; // two stitches into one previous row stitch
+	let targetSingleCrochetStitches = 0;
+	let targetSteps = 0; // a double crochet (inc) counts as one step and two stitches
+	let stitchesPerStep = 1; // >1 to grow, <1 to shrink
+	// A 'seam' or 'dart' occurs where the double or decrementing stitches run from row to row
+	// to prevent this we manage the distance between these stitches so they never appear together.
+	let seamSpread = 9999; // no seam until calculated. This is the ratio of steps to doubles (inc/dec).
+	let seamSpreadPrevRow = previousRowStitches * 2;
+	let seamAdjust = 0; // this is the number of sc to insert at the start of the row
+	let prevSeamAdjust = 0;
+	for (let i = 3; i <= rows; i++) {
+		let targetStitch = 0;
+		let step = 0;
+		let stitch = 0;
+		// determine the circumfrence of this row
+		let progressRatio = i / rows; // how far through are we?
+		let rowRadiusInCm = progressRatio * radiusInCm;
+		console.log("row "+i+": radius "+rowRadiusInCm+" cm");
+		let rowCircumfrenceInCm = 2 * Math.PI * rowRadiusInCm;
+		let rowCircumfrenceInStitches = Math.round(rowCircumfrenceInCm * stitchesPerCm);
+		totalStitches += rowCircumfrenceInStitches
+		
+		
+		
+		// show row and stitches in this row
+		let rowPattern = new CPattern();
+
+		// incrementing - we only increment for a circle
+		targetIncrementStitches = rowCircumfrenceInStitches - previousRowStitches;
+		targetSingleCrochetStitches = previousRowStitches - targetIncrementStitches;
+		targetSteps = targetIncrementStitches + targetSingleCrochetStitches;
+		stitchesPerStep = rowCircumfrenceInStitches / targetSteps;
+		
+		
+		
+		// to prevent darting, rotate the inc/dec over three rows
+		seamSpread = targetSteps / targetIncrementStitches;
+		if (seamSpread >= 3) {
+			seamAdjust = Math.round((seamSpread / 3) * (i % 3));
+		} else {
+			// or over two rows if every other stitch is in/dec 
+			seamAdjust = Math.round((seamSpread / 2) * (i % 2));
+		}
+		
+		// prevent accidental alignment caused by a combination of rotation and a change in stitchSpread (eg when going  from 3 steps to 2 steps near the circumference)
+		// this should only occure near the ends, when the number of stitches in the row is small
+		
+		if (seamAdjust == prevSeamAdjust) {
+			if (seamAdjust > 1) {
+				seamAdjust--;
+			} else {
+				seamAdjust++;
+			}
+		}
+		
+		// prevent a seam adjustment > the number of sc in the row
+		if (seamAdjust > targetSingleCrochetStitches) {
+			seamAdjust = targetSingleCrochetStitches;
+		}
+		
+		// do the seam adjustment
+		// pattern += " % " + Math.round(10*seamSpread)/10 + " % " + Math.round(10*seamAdjust)/10 + " % ";
+		if (seamAdjust >= 1) {
+			for (let j = 1; j <= seamAdjust; j++)  {
+				step++;
+				targetStitch++;// += stitchesPerStep; // to prevent bunching, don't add the full stitchesPerStep
+				rowPattern.PushStitch("sc");
+				stitch++;
+			}
+		}
+
+		
+
+		while (stitch < rowCircumfrenceInStitches) {
+			step++;
+			targetStitch += stitchesPerStep;
+			if (targetStitch > stitch + 1.01) {
+				stitch += 2;
+				rowPattern.PushStitch("inc");
+			} else {
+				stitch++;
+				rowPattern.PushStitch("sc");
+			}
+		}			
+
+		
+		pattern += 
+			`<div id="row-${i}" class="pattern-row" progress="none" onclick="focusRow(event)">
+			row ${i}: `;
+			
+		// pattern +=	" | " + rowCircumfrenceInStitches;
+		pattern += rowPattern.GetPattern() + br;
+		pattern += "</div>";		
+		
+		// setup for next row
+		previousRowStitches = rowCircumfrenceInStitches;
+		seamSpreadPrevRow = seamSpread;
+		prevSeamAdjust = seamAdjust;
+	}
+	pattern += "total stitches: " + totalStitches + br;
+	
+	
+	
+	
+	// estimate total yarn
+	let estimateYarnRequired = totalStitches * testPatternYarnPerStitch;
+	let estimateYarnRequiredGrams = totalStitches / testPatternTotalStitches * testPatternYarnUsedGrams
+	pattern += "Estimated yarn required: " + Math.round(estimateYarnRequired) + " m" + br;
+	pattern += "Estimated yarn required: " + Math.round(estimateYarnRequiredGrams) + " g" + br;
+	
+	return pattern;
+}
+
+function generate() {
+	console.log("generate");
+	
+	// collect test pattern data from the form
+	testPatternWidthInStitches = document.forms["test-pattern"]["test-width-stitches"].value;
+	testPatternWidthMeasure = document.forms["test-pattern"]["test-width-measure"].value;
+	testPatternLengthInRows = document.forms["test-pattern"]["test-length-rows"].value;
+	testPatternLengthMeasure = document.forms["test-pattern"]["test-length-measure"].value;
+	testPatternYarnUsed = document.forms["test-pattern"]["test-yarn-used"].value;
+	testPatternYarnUsedGrams = document.forms["test-pattern"]["test-yarn-used-grams"].value;
+	
+	console.log("Test pattern width: " + testPatternWidthInStitches + " stitches.");
+	console.log("Test pattern width: " + testPatternWidthInStitches + " mm.");
+	console.log("Test pattern length: " + testPatternWidthInStitches + " rows.");
+	console.log("Test pattern length: " + testPatternWidthInStitches + " mm.");
+	console.log("Test pattern yarn used: " + testPatternYarnUsed + " m.");
+	console.log("Test pattern yarn used: " + testPatternYarnUsedGrams + " g.");
+	
+	// calculate ratios from the test pattern
+	stitchesPerCm = testPatternWidthInStitches / testPatternWidthMeasure * 10;
+	rowsPerCm = testPatternLengthInRows / testPatternLengthMeasure * 10;
+	stitchLengthToWidthRatio = (testPatternWidthMeasure / testPatternWidthInStitches) / (testPatternLengthMeasure / testPatternLengthInRows);
+	testPatternTotalStitches = testPatternLengthInRows * testPatternWidthInStitches;
+	testPatternYarnPerStitch = testPatternYarnUsed / testPatternTotalStitches;
+	
+	console.log("Test pattern stitches per cm: " + stitchesPerCm);
+	console.log("Test pattern rows per cm: " + rowsPerCm);
+	console.log("Test pattern stitch length to width ratio: " + stitchLengthToWidthRatio);
+	
+	// get the target shape details
+	// for now, only a sphere. TODO: other shapes
+	shape = document.forms["shape"]["shape"].value;
+	shapeRadius = document.forms["shape"]["radius"].value;
+	
+	// generate the pattern
+	let pattern = "";
+	switch (shape) {
+		case "sphere":
+			pattern = generateSphere();
+			break;
+		case "circle":
+			pattern = generateCircle();
+			break;
+		default: 
+			pattern = "Error: shape not recognised.";
+	}
+	
+	// put the pattern on the panel
+	let patternPanel = document.getElementById("pattern");
 	patternPanel.innerHTML = pattern;
 	patternPanel.style.display = "block";	
 }
